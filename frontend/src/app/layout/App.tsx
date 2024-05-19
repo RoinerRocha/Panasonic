b89fbe1b44dac65606f6b2a144d5bce73b12a6d1
RoinerRocha <rochabozaroiner@gmail.com>
2024-05-19 12:24:04 -0600

    add appBar

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -1,50 +1,33 @@
 import logo from './logo.svg';
 import React, { useEffect, useState } from 'react';
 import api from '../api/api';
+import Header from './header';
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
-const App: React.FC = () => {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.get('/');
-        setMessage(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+function App(){
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
       }
-    };
-    fetchData();
-  }, []);
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Container>
         <p>Hola Mundo</p>
-      </header>
-    </div>
+      </Container>
+    </ThemeProvider>
   );
 };
 
