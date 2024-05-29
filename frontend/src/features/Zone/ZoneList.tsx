6cdd9b4913636806ab8b28bf7be585741a8b2bd9
RoinerRocha <rochabozaroiner@gmail.com>
2024-05-29 14:48:12 -0600

    Tables solution

--- frontend/src/features/Zone/ZoneList.tsx
+++ frontend/src/features/Zone/ZoneList.tsx
@@ -1,6 +1,5 @@
-import { Grid } from "@mui/material";
+import { Grid, TableContainer, Paper, Table, TableCell, TableHead, TableRow, TableBody, Button } from "@mui/material";
 import { Zona } from "../../app/models/zone";
-import ZoneTable from "./ZoneTable";
 
 interface Props{
     zonas: Zona[];
@@ -8,12 +7,32 @@ interface Props{
 
 export default function ZoneList({zonas}:Props) {
     return (
-        <Grid container spacing={4}>
-            {zonas.map(zona => (
-                <Grid item xs={12} key={zona.id}>
-                    <ZoneTable zona={zona} />
-                </Grid>
-            ))}
+        <Grid container spacing={1}>
+            <TableContainer  component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Numero</TableCell>
+                            <TableCell align="center">Nombre</TableCell>
+                            <TableCell align="center">Encargado</TableCell>
+                            <TableCell align="center" >Configuracion</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {zonas.map(zona => (
+                            <TableRow key={zona.id}>
+                                <TableCell align="center">{zona.numeroZona}</TableCell>
+                                <TableCell align="center">{zona.nombreZona}</TableCell>
+                                <TableCell align="center">{zona.responsableAreaNom_user}</TableCell>
+                                <TableCell align='center' >
+                                    <Button variant='contained' color='info' sx={{ margin: '0 8px' }}>editar</Button>
+                                    <Button variant='contained' color='error' sx={{ margin: '0 8px' }}>Eliminar</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Grid>
     )
 }
