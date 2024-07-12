197bfa392b13f4e70aee3dd2f5901aff2d15a76a
RoinerRocha <rochabozaroiner@gmail.com>
2024-07-12 15:36:44 -0600

    search bar and decimals

--- frontend/src/app/models/depreciationModel.ts
+++ frontend/src/app/models/depreciationModel.ts
@@ -6,3 +6,12 @@ export interface depreciationModel {
     Colones: number;
     Clasificacion: string;
 }
+
+export interface depreciationFormModel {
+    id: number;
+    Codigo: string;
+    Cuenta: string;
+    Dolares: string; // Aquí es string en lugar de number
+    Colones: string; // Aquí es string en lugar de number
+    Clasificacion: string;
+}
