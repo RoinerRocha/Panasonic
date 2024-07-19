0c53351bf7bbade781164e58bfc84ebd91eb4234
RoinerRocha <rochabozaroiner@gmail.com>
2024-07-19 12:22:09 -0600

    images

--- frontend/src/app/layout/SigninMenu.tsx
+++ frontend/src/app/layout/SigninMenu.tsx
@@ -3,17 +3,26 @@ import React from "react";
 import { useAppDispatch, useAppSelector } from "../../store/configureStore";
 import { signOut } from "../../features/account/accountSlice";
 import { Link } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
+import { useLanguage } from '../../app/context/LanguageContext';
 
 export default function SignInMenu() {
   const dispatch = useAppDispatch();
   const {user} = useAppSelector(state => state.account);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+  const { t } = useTranslation();
+  const { changeLanguage, language } = useLanguage();
+
   const handleClick = (event: any) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleChangeLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    changeLanguage(event.target.value);
   };
   console.log('nombre de usuario:', user?.nombre_usuario);
   return (
@@ -31,10 +40,9 @@ export default function SignInMenu() {
         onClose={handleClose}
         TransitionComponent={Fade}
       >
-        <MenuItem onClick={handleClose}>Perfil</MenuItem>
-        <MenuItem onClick={handleClose}>Editar</MenuItem>
-        <MenuItem component={Link} to="/users" onClick={handleClose}>Registro</MenuItem>
-        <MenuItem onClick={() =>dispatch(signOut())}>Cerrar sesión</MenuItem>
+        {/* <MenuItem onClick={handleClose}>Editar</MenuItem> */}
+        <MenuItem component={Link} to="/users" onClick={handleClose}>{t('signMenu-registro')}</MenuItem>
+        <MenuItem onClick={() =>dispatch(signOut())}>{t('logout')}</MenuItem>
       </Menu>
     </div>
   );
